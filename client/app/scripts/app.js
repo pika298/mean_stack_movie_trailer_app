d8f8b59a7e09ea8850bda4b687d240ef74f73ff8
pika298 <kit2008@live.com>
2016-10-13 16:14:02 +0800

    add CRUD movies functions

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,9 +10,13 @@
  */
 angular
   .module('clientApp', [
-    'ngRoute'
+    'ngRoute',
+    'restangular'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -24,7 +28,42 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl',
+        controllerAs: 'movie'
+      })
+      .when('/create/movie', {
+        templateUrl: 'views/movie-add.html',
+        controller: 'MovieAddCtrl',
+        controllerAs: 'movieAdd'
+      })
+      .when('/movie/:id', {
+        templateUrl: 'views/movie-view.html',
+        controller: 'MovieViewCtrl',
+        controllerAs: 'movieView'
+      })
+      .when('/movie/:id/delete', {
+        templateUrl: 'views/movie-delete.html',
+        controller: 'MovieDeleteCtrl',
+        controllerAs: 'movieDelete'
+      })
+      .when('/movie/:id/edit', {
+        templateUrl: 'views/movie-edit.html',
+        controller: 'MovieEditCtrl',
+        controllerAs: 'movieEdit'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .factory('MovieRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setRestangularFields({
+        id: '_id'
+      });
+    });
+  })
+  .factory('Movie', function(MovieRestangular) {
+    return MovieRestangular.service('movie');
   });
